d352d50c8544f90afaa3dafa0e477a20c81f4c43
seonhoshin <sunlike0508@gmail.com>
2024-09-09 21:05:02 +0900

    주석

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -124,6 +124,13 @@ public class BasicController {
     }
 
 
+    @GetMapping("/comments")
+    public String comments(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/comments";
+    }
+
+
     private void addUsers(Model model) {
         List<User> users = new ArrayList<>();
         users.add(new User("userA", 10));
